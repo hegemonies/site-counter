229fe6b0b013023de5004948f139fde828d6661e
daniil dyachenko <hegemonies@yandex.ru>
2023-02-01 09:45:47 +0400

    Fixes controller

---
+++ src/main/kotlin/site/hegemonies/sitecounter/configuration/CoroutineDispatcherConfiguration.kt
@@ -0,0 +1,18 @@
+package site.hegemonies.sitecounter.configuration
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import kotlin.coroutines.CoroutineContext
+
+@Configuration
+class CoroutineDispatcherConfiguration {
+
+    @Bean
+    fun coroutineScope(): CoroutineScope =
+        object : CoroutineScope {
+            override val coroutineContext: CoroutineContext = Dispatchers.IO.limitedParallelism(10)
+        }
+}
