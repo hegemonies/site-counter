e778fe60c62000f2566d5ec7571a3a97c0dc8f08
daniil dyachenko <hegemonies@yandex.ru>
2023-07-15 18:27:38 +0400

    Updates deps

--- collector/src/main/kotlin/site/hegemonies/collector/CollectorApplication.kt
+++ collector/src/main/kotlin/site/hegemonies/collector/CollectorApplication.kt
@@ -1,15 +1,9 @@
 package site.hegemonies.collector
 
-import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.EnableKafka
-import org.springframework.stereotype.Component
-import site.hegemonies.collector.dto.CounterEventDto
-import site.hegemonies.collector.service.CounterService
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -17,25 +11,5 @@ import site.hegemonies.collector.service.CounterService
 class CollectorApplication
 
 fun main(args: Array<String>) {
-	runApplication<CollectorApplication>(*args)
-}
-
-
-@Component
-class Foo(
-	private val counterService: CounterService
-) {
-
-	@EventListener(ApplicationReadyEvent::class)
-	fun foo() = runBlocking {
-		val event = CounterEventDto(
-			id = 1,
-			clientAddress = "1.2.3.4",
-			uri = "https://hegemonies.site/cv",
-			headers = mapOf("os" to "macOS"),
-			createdAt = System.currentTimeMillis()
-		)
-
-		counterService.saveCounter(event)
-	}
+    runApplication<CollectorApplication>(*args)
 }
